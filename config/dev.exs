112e9568ebf2a246fa397522de1d10b2f5236fae
Hans Hoffman <hoffman.hans.j@gmail.com>
2022-03-27 15:11:31 -0600

    setup: TailwindCSS

--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :huzzah, HuzzahWeb.Endpoint,
   secret_key_base: "AKUcZn8/lwmtmRn3psiIfsyZKdUiTCLJJjlBAyVoa4YSvRp05Q07x4R4eVDuwvj2",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
